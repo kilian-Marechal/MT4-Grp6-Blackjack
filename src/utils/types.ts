331504f7d278e18efe9148ab56101d217e966d04
Kilian <marechal.kilian@gmail.com>
2022-09-12 13:58:31 +0200

    test string type

--- src/utils/types.ts
+++ src/utils/types.ts
@@ -4,9 +4,9 @@ import { Server as SocketIOServer } from "socket.io";
 
 export type Color = "black" | "red";
 
-export type Rank = 'A' | '2' | '3' | '4' | '5' | '6' | '7' | '8' | '9' | '10' | 'J' | 'Q' | 'K'
+export type Rank = 'A' | '2' | '3' | '4' | '5' | '6' | '7' | '8' | '9' | '10' | 'J' | 'Q' | 'K' | string
 
-export type Suit = 'Diamonds' | 'Hearts' | 'Spades' | 'Clubs'
+export type Suit = 'Diamonds' | 'Hearts' | 'Spades' | 'Clubs' | string
 
 export type NextApiResponseServerIO = NextApiResponse & {
   socket: Socket & {
